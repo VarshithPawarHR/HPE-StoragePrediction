5fa9a7d688674de39e91e4f56f1faaa7df1b50c0
Varshith Pawar H R <varshithpawarhr@gmail.com>
2025-04-29 16:01:19 +0530

    clean UP

--- Collab/DeetyaSSalian-LSTM/backend/app/schemas.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from typing import Dict
-
-class PredictionResponse(BaseModel):
-    one_day: Dict[str, float]
-    one_week: Dict[str, float]
-    one_month: Dict[str, float]
-    three_months: Dict[str, float]
