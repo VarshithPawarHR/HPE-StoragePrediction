3f9f427229341da2525db6d3e7e07432e347cca7
Varshith Pawar H R <varshithpawarhr@gmail.com>
2025-04-30 22:46:19 +0530

    feat: growth rate API

--- backend/main.py
+++ backend/main.py
@@ -9,6 +9,7 @@ from fastapi.responses import JSONResponse
 from zoneinfo import ZoneInfo
 from fastapi.middleware.cors import CORSMiddleware
 
+
 IST = ZoneInfo("Asia/Kolkata")
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -33,12 +34,14 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+# Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Allow all origins
     allow_credentials=True,
-    allow_methods=["*"],  # Allow all HTTP methods
-    allow_headers=["*"],  # Allow all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
@@ -93,3 +96,39 @@ async def get_directory_usage(
         "directory": directory,
         "data": formatted
     })
+
+
+@app.get("/growth-rate")
+async def get_growth_rate(
+    directory: str = Query(..., description="Directory name, e.g. /scratch")
+):
+    cursor = collection.find({
+        "directory": directory
+    }).sort("timestamp", DESCENDING).limit(96)
+
+    results = await cursor.to_list(length=96)
+
+    if len(results) < 2:
+        return JSONResponse(
+            {"error": "Not enough data to calculate growth rate."}, status_code=400
+        )
+
+    first_entry = results[0]["storage_gb"]
+    last_entry = results[-1]["storage_gb"]
+
+    if last_entry == 0:
+        return JSONResponse(
+            {"error": "Last entry storage is 0, cannot divide by zero."}, status_code=400
+        )
+
+    growth_rate = (first_entry - last_entry)  / last_entry
+    growth_rate = growth_rate*100  # Convert to percentage
+
+    return JSONResponse({
+        "directory": directory,
+        "first_entry": first_entry,
+        "last_entry": last_entry,
+        "growth_rate_percent": round(growth_rate, 2)
+    })
+
+
